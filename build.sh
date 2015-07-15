81aee21156e1bb185908d20437a2ee5abceaf6a9
Matt Shelton <mshelton@isightpartners.com>
2015-07-15 15:01:49 -0400

    Adding a script that will install all the dependencies and build the RPM.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Install packages
+yum -y install rpm-build libnfnetlink-devel libyaml-devel libnetfilter_queue-devel libnet-devel zlib-devel libpcap-devel pcre-devel libcap-ng-devel nspr-devel nss-devel nss-softokn-devel file-devel jansson-devel GeoIP-devel python2-devel lua-devel systemd autoconf automake libtool gcc gcc-c++
+
+# Download the Suricata source code
+wget http://www.openinfosecfoundation.org/download/suricata-2.0.8.tar.gz
+mv suricata-2.0.8.tar.gz suricata/
+
+# Copy all of the RPM files to the appropriate directory.
+if [ ! -e "${HOME}/rpmbuild/SOURCES" ]; then
+    mkdir ${HOME}/rpmbuild/SOURCES
+fi
+cp suricata/* ${HOME}/rpmbuild/SOURCES/
+
+# Build the RPMs
+rpmbuild -bb suricata/suricata.spec
+
