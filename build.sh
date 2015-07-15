0dbe730a5447a9fa9b0aa92da14d48f8d6493a0d
Matt Shelton <matthew.j.shelton@gmail.com>
2015-07-15 16:22:57 +0000

    Added lua devel package.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install packages
-sudo yum -y install rpm-build libnfnetlink-devel libyaml-devel libnetfilter_queue-devel libnet-devel zlib-devel libpcap-devel pcre-devel libcap-ng-devel nspr-devel nss-devel nss-softokn-devel file-devel jansson-devel GeoIP-devel python2-devel lua-devel autoconf automake libtool gcc gcc-c++
+sudo yum -y install rpm-build libnfnetlink-devel libyaml-devel libnetfilter_queue-devel libnet-devel zlib-devel libpcap-devel pcre-devel libcap-ng-devel nspr-devel nss-devel nss-softokn-devel file-devel jansson-devel GeoIP-devel python2-devel lua-devel autoconf automake libtool gcc gcc-c++ lua-devel 
 
 # Download the Suricata source code
 wget http://www.openinfosecfoundation.org/download/suricata-2.0.8.tar.gz
