9237a69b42e75d063d0bb17ff00997162aafed9b
Matt Shelton <matthew.j.shelton@gmail.com>
2015-07-15 15:37:26 +0000

    Removed an unnecessary package from installation.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install packages
-yum -y install rpm-build libnfnetlink-devel libyaml-devel libnetfilter_queue-devel libnet-devel zlib-devel libpcap-devel pcre-devel libcap-ng-devel nspr-devel nss-devel nss-softokn-devel file-devel jansson-devel GeoIP-devel python2-devel lua-devel systemd autoconf automake libtool gcc gcc-c++
+sudo yum -y install rpm-build libnfnetlink-devel libyaml-devel libnetfilter_queue-devel libnet-devel zlib-devel libpcap-devel pcre-devel libcap-ng-devel nspr-devel nss-devel nss-softokn-devel file-devel jansson-devel GeoIP-devel python2-devel lua-devel autoconf automake libtool gcc gcc-c++
 
 # Download the Suricata source code
 wget http://www.openinfosecfoundation.org/download/suricata-2.0.8.tar.gz
@@ -9,7 +9,7 @@ mv suricata-2.0.8.tar.gz suricata/
 
 # Copy all of the RPM files to the appropriate directory.
 if [ ! -e "${HOME}/rpmbuild/SOURCES" ]; then
-    mkdir ${HOME}/rpmbuild/SOURCES
+    mkdir -p ${HOME}/rpmbuild/SOURCES
 fi
 cp suricata/* ${HOME}/rpmbuild/SOURCES/
 
